c08b0990d8f83007711d2719b7c9228bbeabbb06
Hemanth <hemanthindhu@gmail.com>
2020-11-19 09:41:15 +0530

    added foreground_extration

--- task-2-synthetic-dataset-gen/README.md
+++ task-2-synthetic-dataset-gen/README.md
@@ -8,8 +8,8 @@ First, create a virtual environment using ``` virtualenv env ``` and activate th
 
 Now, download the RetinaNet model file using the following [link](https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/resnet50_coco_best_v2.0.1.h5)
 
-
-
 After that just run the ```requirements.txt```    file using ```pip install -r requirements.txt``` to install all the necessary libraries.
 
 Now, execute the code using ```python main.py```
+
+
--- task-2-synthetic-dataset-gen/main.py
+++ task-2-synthetic-dataset-gen/main.py
@@ -1,8 +1,10 @@
 from imageai.Detection import ObjectDetection
 import os
 import cv2
+import foreground_extration as fe
 
-INPUT_IMAGE = 'dog.jpg'
+
+INPUT_IMAGE = 'dog_1.jpg'
 OUTPUT_IMAGE = 'dog_op_1.jpg'
 
 def get_rect():
@@ -16,6 +18,10 @@ def get_rect():
 
 def main():
 	detections = get_rect()
-	print(f"Name = {detections[0]['name']}\nCo-ordinates = {detections[0]['box_points']}")
+	# print(f"Name = {detections[0]['name']}\nCo-ordinates = {detections[0]['box_points']}")
+	image = fe.extract(INPUT_IMAGE,detections[0]['box_points'])
+
+	cv2.imshow("Extracted Image", image)
+	cv2.waitKey(0)
 
 main()
